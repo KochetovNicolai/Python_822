109f04ad9389279c59be769861447e6de64a2363
alniterentev <terentev.an@phystech.edu>
2019-03-23 22:09:24 +0300

    reworked some parts. made the game full(or not?)

--- terentyevalexey/review_1/Game.py
+++ terentyevalexey/review_1/Game.py
@@ -1,7 +1,8 @@
-"""Игра про кота-качка. Кот хочет накачаться, помоги ему!
+"""
+Тамагочи про кота-качка. Кот хочет накачаться, помоги ему!
 Коту нужно бить грушу, чтобы качать руки и ноги.
 Так же коту нужно бегать, чтобы не перенапрягать мышцы,
-а ещё ему необходимо отдыхать, чтобы не умереть.
+а ещё ему необходимо отдыхать и спать, чтобы не умереть.
 """
 
 import random
@@ -9,151 +10,259 @@ import pygame
 import sys
 import os
 
-Full_screen = False
-Game_name = "Game"
-pygame.init()
-pygame.display.set_caption(Game_name)
-display_inf = pygame.display.Info()
-width = display_inf.current_w // 20
-height = display_inf.current_h // 20
-screen = pygame.display.set_mode((width * 10, height * 10))
-cat = pygame.image.load(os.path.join('data', 'cat.png'))
+logo = pygame.image.load(os.path.join('data', 'logo.png'))
 background = pygame.image.load(os.path.join('data', 'background.jpg'))
 arrow = pygame.image.load(os.path.join('data', 'arrow.png'))
 bamboo = pygame.image.load(os.path.join('data', 'bamboo.jpg'))
 
+pygame.init()
+pygame.display.set_caption("cat")
+pygame.display.set_icon(logo)
+display_inf = pygame.display.Info()
+width = display_inf.current_w // 20
+height = display_inf.current_h // 20
+full_screen = False
+screen = pygame.display.set_mode((width * 10, height * 10))
+
 tick_rate = 30
-anim_time = 0
 
-cat_x, cat_y = width * 4, height * 5
-cat_cardio, cat_muscle, cat_tired = 0, 0, 0
 
-animations = {
-    "idle": [
-        pygame.image.load(os.path.join('data', 'cat', 'idle_{}.png'.format(i)))
-        for i in range(1, 5)],
-    "dead": [
-        pygame.image.load(os.path.join('data', 'cat', 'dead_{}.png'.format(i)))
-        for i in range(1, 8)],
-    "kick": [
-        pygame.image.load(os.path.join('data', 'cat', 'kick_{}.png'.format(i)))
-        for i in range(1, 9)],
-    "punch": [
-        pygame.image.load(os.path.join('data', 'cat', 'punch_{}.png'.format(i)))
-        for i in range(1, 7)],
-    "walk": [
-        pygame.image.load(os.path.join('data', 'cat', 'walk_{}.png'.format(i)))
-        for i in range(1, 9)],
-    "walk_left": [
-        pygame.image.load(
-            os.path.join('data', 'cat', 'walk_left_{}.png'.format(i)))
-        for i in range(1, 9)]
-}
+class Cat:
+    name = "Kitty"
+    x = width * 4
+    y = height * 5
+    anim_time = 0
+    idle_time = 0
+
+    cardio = 0
+    muscle = 0
+    tired = 0
+    sleepy = 0
+
+    animations = {
+        "idle": [
+            pygame.image.load(
+                os.path.join('data', 'cat', 'idle_{}.png'.format(i)))
+            for i in range(1, 5)],
+        "dead": [
+            pygame.image.load(
+                os.path.join('data', 'cat', 'dead_{}.png'.format(i)))
+            for i in range(1, 8)],
+        "kick": [
+            pygame.image.load(
+                os.path.join('data', 'cat', 'kick_{}.png'.format(i)))
+            for i in range(1, 9)],
+        "punch": [
+            pygame.image.load(
+                os.path.join('data', 'cat', 'punch_{}.png'.format(i)))
+            for i in range(1, 7)],
+        "walk": [
+            pygame.image.load(
+                os.path.join('data', 'cat', 'walk_{}.png'.format(i)))
+            for i in range(1, 9)],
+        "walk_left": [
+            pygame.image.load(
+                os.path.join('data', 'cat', 'walk_left_{}.png'.format(i)))
+            for i in range(1, 9)],
+        "sleep": [
+            pygame.image.load(
+                os.path.join('data', 'cat', 'sleep_{}.png'.format(i)))
+            for i in range(1, 8)]
+    }
+
+
+def cat_reset():
+    Cat.name = "Kitty"
+    Cat.x = width * 4
+    Cat.y = height * 5
+    Cat.anim_time = 0
+    Cat.idle_time = 0
+    Cat.cardio = 0
+    Cat.muscle = 0
+    Cat.tired = 0
+    Cat.sleepy = 0
+
+
+def sleep():
+    Cat.anim_time = 0
+    clock = pygame.time.Clock()
+    for _ in range(tick_rate // 7 * 7):
+        clock.tick(tick_rate)
+        screen.blit(
+            pygame.transform.scale(background, (width * 10, height * 10)),
+            (0, 0))
+        draw_score()
+        draw_cat("sleep")
+    for cur_time in range(10 * tick_rate):
+        clock.tick(tick_rate)
+        if cur_time % tick_rate == 0:
+            if Cat.sleepy > 0:
+                Cat.sleepy -= 1
+                if cur_time % (2 * tick_rate) == 0 and Cat.tired > 0:
+                    Cat.tired -= 1
+            else:
+                return
+        screen.blit(
+            pygame.transform.scale(background, (width * 10, height * 10)),
+            (0, 0))
+        draw_score()
+        screen.blit(pygame.transform.scale(Cat.animations["sleep"][6],
+                                           (width * 4, height * 4)),
+                    (Cat.x, Cat.y))
+        pygame.display.update()
+        pygame.event.clear()
 
 
 def run_cat_run():
-    global cat_x, cat_y, cat_cardio, cat_tired, anim_time
-    anim_time = 0
-    cat_x = 0
-    cat_y = 6.7 * height
+    Cat.anim_time = 0
+    Cat.x = 0
+    Cat.y = 6.7 * height
     cur_x = 0
     clock = pygame.time.Clock()
-    for _ in range(153):
+    for _ in range(200):
         clock.tick(tick_rate)
         screen.blit(pygame.transform.scale(bamboo, (height * 48, height * 10)),
                     (0, 0), (cur_x, 0, cur_x + 2133, 1200))
         draw_cat("walk")
-        cat_x += 5
-        cur_x += 5
+        Cat.x += width // 20
+        cur_x += width // 20
 
     screen.blit(pygame.transform.scale(background, (width * 10, height * 10)),
                 (0, 0))
-    cat_x = 4 * width
-    cat_y = 5 * height
-    anim_time = 0
-    cat_cardio += 2
-    cat_tired += 1
+
+    Cat.x = 4 * width
+    Cat.y = 5 * height
+    Cat.anim_time = 0
+
+    Cat.cardio += 2
+    Cat.tired += 1
 
 
 def punching_bag():
-    global anim_time, cat_muscle, cat_tired
-    anim_time = 0
+    Cat.anim_time = 0
     clock = pygame.time.Clock()
-    for _ in range(random.randint(10, 15) * 7):
+    for _ in range(random.randint(2, 4) * tick_rate):
         clock.tick(tick_rate)
         screen.blit(
             pygame.transform.scale(background, (width * 10, height * 10)),
             (0, 0))
         draw_score()
         draw_cat("punch")
-    cat_muscle += 3
-    cat_tired += 2
+
+    Cat.muscle += 3
+    Cat.tired += 2
 
 
 def kicking_bag():
-    global anim_time, cat_muscle, cat_tired
-    anim_time = 0
+    Cat.anim_time = 0
     clock = pygame.time.Clock()
-    for _ in range(random.randint(10, 15) * 9):
+    for _ in range(random.randint(2, 4) * tick_rate):
         clock.tick(tick_rate)
         screen.blit(
             pygame.transform.scale(background, (width * 10, height * 10)),
             (0, 0))
         draw_score()
         draw_cat("kick")
-    cat_muscle += 2
-    cat_tired += 2
+
+    Cat.muscle += 2
+    Cat.tired += 2
+
+
+def die():
+    Cat.anim_time = 0
+    clock = pygame.time.Clock()
+    for _ in range(tick_rate // 7 * 7):
+        clock.tick(tick_rate)
+        screen.blit(
+            pygame.transform.scale(background, (width * 10, height * 10)),
+            (0, 0))
+        draw_score()
+        draw_cat("dead")
+
+    font = pygame.font.SysFont("calibri", height, bold=True)
+    text = font.render("{} is dead".format(Cat.name), True, (32, 32, 32))
+    text_rect = text.get_rect()
+    text_rect.center = (width * 5.5, height * 9)
+    screen.blit(text, text_rect)
+
+    font = pygame.font.SysFont("calibri", height // 2, bold=True)
+    text = font.render("PRESS ANY KEY TO CONTINUE", True, (32, 32, 32))
+    text_rect = text.get_rect()
+    text_rect.center = (width * 5.5, height * 9.7)
+    screen.blit(text, text_rect)
+
+    pygame.display.update()
+
+    cat_reset()
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            if event.type == pygame.KEYDOWN:
+                return main_menu()
 
 
 def draw_cat(status):
-    global anim_time
+    if Cat.anim_time >= tick_rate:
+        Cat.anim_time = 0
 
-    anim_time += 1
-    if anim_time >= tick_rate:
-        anim_time = 0
-
-    cur_frame = anim_time // (
-            (tick_rate - tick_rate % len(animations[status])) //
-            len(animations[status])) + 1
-    if cur_frame >= len(animations[status]):
-        anim_time = 0
+    cur_frame = Cat.anim_time // (
+            tick_rate //
+            len(Cat.animations[status]))
+    Cat.anim_time += 1
+    if cur_frame >= len(Cat.animations[status]):
+        Cat.anim_time = 0
         return
-    screen.blit(pygame.transform.scale(animations[status][cur_frame],
+    screen.blit(pygame.transform.scale(Cat.animations[status][cur_frame],
                                        (width * 4, height * 4)),
-                (cat_x, cat_y))
+                (Cat.x, Cat.y))
     pygame.display.update()
 
 
 def draw_score():
     font = pygame.font.SysFont("calibri", height // 2, bold=True)
 
-    text = font.render(str(cat_tired), True, (32, 32, 32))
+    text = font.render(str(Cat.tired), True, (32, 32, 32))
     text_rect = text.get_rect()
     text_rect.center = (width * 4, height * 4.35)
     screen.blit(text, text_rect)
 
-    text = font.render(str(cat_muscle), True, (32, 32, 32))
+    text = font.render(str(Cat.muscle), True, (32, 32, 32))
     text_rect = text.get_rect()
     text_rect.center = (width * 5, height * 4.05)
     screen.blit(text, text_rect)
 
-    text = font.render(str(cat_cardio), True, (32, 32, 32))
+    text = font.render(str(Cat.cardio), True, (32, 32, 32))
     text_rect = text.get_rect()
-    text_rect.center = (width * 6, height * 4.35)
+    text_rect.center = (width * 5.975, height * 4.35)
+    screen.blit(text, text_rect)
+
+    text = font.render(str(Cat.sleepy), True, (32, 32, 32))
+    text_rect = text.get_rect()
+    text_rect.center = (width * 6.975, height * 4.05)
     screen.blit(text, text_rect)
 
 
 def main():
-    screen.fill((0, 0, 0))
     screen.blit(pygame.transform.scale(background, (width * 10, height * 10)),
                 (0, 0))
     clock = pygame.time.Clock()
 
-    global anim_time, cat_x, cat_y, cat_tired, cat_muscle, cat_cardio
     status = "idle"
+    Cat.idle_time = 0
+    pygame.time.set_timer(pygame.USEREVENT + 1, 6000)  # cat sleepy inc
+    pygame.time.set_timer(pygame.USEREVENT + 2, 8000)  # cat muscle dec
+    pygame.time.set_timer(pygame.USEREVENT + 3, 12000)  # cat cardio dec
 
     while True:
         clock.tick(tick_rate)
+
+        if Cat.muscle > 30:
+            Cat.muscle -= 1
+        if Cat.cardio > 30:
+            Cat.cardio -= 1
 
         screen.blit(
             pygame.transform.scale(background, (width * 10, height * 10)),
@@ -161,21 +270,27 @@ def main():
         draw_score()
         draw_cat(status)
 
-        pygame.time.get_ticks()
+        if Cat.sleepy >= 20:
+            sleep()
+        if Cat.tired > 10:
+            return die()
 
-        if cat_muscle > 30:
-            cat_muscle -= 1
-        if cat_cardio > 30:
-            cat_cardio -= 1
-        if cat_tired > 20:
-            die()
         for event in pygame.event.get():
+            if event.type == pygame.USEREVENT + 1:
+                Cat.sleepy += 1
+            if event.type == pygame.USEREVENT + 2:
+                if Cat.muscle > 0:
+                    Cat.muscle -= 1
+            if event.type == pygame.USEREVENT + 3:
+                if Cat.cardio > 0:
+                    Cat.cardio -= 1
+
             if event.type == pygame.QUIT:
                 pygame.quit()
                 sys.exit()
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_ESCAPE:
-                    return
+                    return main_menu()
                 elif pygame.key.get_mods() & pygame.KMOD_ALT and \
                         event.key == pygame.K_F4:
                     pygame.quit()
@@ -185,69 +300,81 @@ def main():
                     run_cat_run()
 
                 elif event.key == pygame.K_SPACE:
-                    cat_x = width * 6.8
+                    Cat.x = width * 6.8
                     if random.randint(1, 2) == 1:
                         punching_bag()
                     else:
                         kicking_bag()
                     status = "idle"
-                    cat_x = width * 4
-                    anim_time = 0
+                    Cat.x = width * 4
+                    Cat.anim_time = 0
+                    Cat.idle_time = 0
 
             if event.type == pygame.MOUSEBUTTONDOWN:
                 if pygame.mouse.get_pressed()[0] == 1:
                     mx, my = pygame.mouse.get_pos()
                     if (mx > width * 9) and (
                             height * 6 < my < height * 9.5):
-                        cat_x = width * 6.8
+                        Cat.x = width * 6.8
                         if random.randint(1, 2) == 1:
                             punching_bag()
                         else:
                             kicking_bag()
                         status = "idle"
-                        cat_x = width * 4
-                        anim_time = 0
+                        Cat.x = width * 4
+                        Cat.anim_time = 0
+                        Cat.idle_time = 0
 
         keys = pygame.key.get_pressed()
         if keys[pygame.K_RIGHT]:
             if status != "walk":
-                anim_time = 0
+                Cat.anim_time = 0
                 status = "walk"
-            cat_x += 10
+            Cat.x += width // 10
 
         elif keys[pygame.K_LEFT]:
             if status != "walk_left":
-                anim_time = 0
+                Cat.anim_time = 0
                 status = "walk_left"
-            cat_x -= 10
+            Cat.x -= width // 10
 
         elif status != "idle":
             status = "idle"
-            anim_time = 0
+            Cat.anim_time = 0
+            Cat.idle_time = 0
 
-        if cat_x >= width * 6.8:
-            cat_x = width * 6.8
+        if status == "idle":
+            Cat.idle_time += 1
+            if Cat.idle_time == tick_rate * 5 and Cat.tired > 0:
+                Cat.tired -= 1
+                Cat.idle_time = 0
+
+        if Cat.x >= width * 6.8:
+            Cat.x = width * 6.8
             if random.randint(1, 2) == 1:
                 punching_bag()
             else:
                 kicking_bag()
             status = "idle"
-            cat_x = width * 4
-            anim_time = 0
+            Cat.x = width * 4
+            Cat.anim_time = 0
+            Cat.idle_time = 0
 
-        if cat_x <= - 2 * width:
+        if Cat.x <= - 2 * width:
             run_cat_run()
             status = "idle"
-            cat_x = width * 4
-            anim_time = 0
+            Cat.x = width * 4
+            Cat.anim_time = 0
+            Cat.idle_time = 0
+
+        pygame.event.clear()
 
 
-# draws main menu
 def draw_menu(pos=0):
     screen.fill((32, 32, 32))
     # draw game name
     font = pygame.font.SysFont("calibri", height, bold=True)
-    text = font.render(Game_name, True, (100, 100, 100))
+    text = font.render(Cat.name, True, (100, 100, 100))
     text_rect = text.get_rect()
     text_rect.center = (width * 5, height * 1.5)
     screen.blit(text, text_rect)
@@ -313,7 +440,7 @@ def draw_options(pos=0):
 def options():
     cur_pose = 0
     draw_options()
-    global screen, Full_screen
+    global screen, full_screen
     while True:
         for event in pygame.event.get():
             if event.type == pygame.MOUSEBUTTONDOWN:
@@ -321,19 +448,19 @@ def options():
                     mx, my = pygame.mouse.get_pos()
                     if (width * 6 > mx > width * 4) and (
                             height * 4.5 > my > height * 3.5):
-                        if not Full_screen:
+                        if not full_screen:
                             screen = pygame.display.set_mode(
                                 (width * 10, height * 10), pygame.FULLSCREEN)
-                            Full_screen = True
+                            full_screen = True
                         else:
                             screen = pygame.display.set_mode(
                                 (width * 10, height * 10))
-                            Full_screen = False
+                            full_screen = False
                         draw_options()
                         pygame.display.update()
                     elif (width * 6 > mx > width * 4) and (
                             height * 6.5 > my > height * 5.5):
-                        return
+                        return main_menu()
 
             if event.type == pygame.QUIT:
                 pygame.quit()
@@ -352,20 +479,20 @@ def options():
                         event.key == pygame.K_KP_ENTER or \
                         event.key == pygame.K_SPACE:
                     if cur_pose == 0:
-                        if not Full_screen:
+                        if not full_screen:
                             screen = pygame.display.set_mode(
                                 (width * 10, height * 10), pygame.FULLSCREEN)
-                            Full_screen = True
+                            full_screen = True
                         else:
                             screen = pygame.display.set_mode(
                                 (width * 10, height * 10))
-                            Full_screen = False
+                            full_screen = False
                         draw_options()
                         pygame.display.update()
                     elif cur_pose == 1:
-                        return
+                        return main_menu()
                 if event.key == pygame.K_ESCAPE:
-                    return
+                    return main_menu()
                 if pygame.key.get_mods() & pygame.KMOD_ALT and \
                         event.key == pygame.K_F4:
                     pygame.quit()
@@ -382,16 +509,10 @@ def main_menu():
                     mx, my = pygame.mouse.get_pos()
                     if (width * 6 > mx > width * 4) and (
                             height * 4 > my > height * 3):
-                        main()
-                        cur_pose = 0
-                        draw_menu()
-                        continue
+                        return main()
                     elif (width * 6 > mx > width * 4) and (
                             height * 6 > my > height * 5):
-                        options()
-                        cur_pose = 0
-                        draw_menu()
-                        continue
+                        return options()
                     elif (width * 6 > mx > width * 4) and (
                             height * 8 > my > height * 7):
                         pygame.quit()
@@ -410,14 +531,9 @@ def main_menu():
                         event.key == pygame.K_KP_ENTER or \
                         event.key == pygame.K_SPACE:
                     if cur_pose == 0:
-                        main()
-                        draw_menu()
-                        continue
+                        return main()
                     elif cur_pose == 1:
-                        options()
-                        cur_pose = 0
-                        draw_menu()
-                        continue
+                        return options()
                     elif cur_pose == 2:
                         pygame.quit()
                         sys.exit()
