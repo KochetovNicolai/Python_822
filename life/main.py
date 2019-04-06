69171c908d13290a8b8daff05df4797113d3c134
zemerov <37634929+zemerov@users.noreply.github.com>
2019-04-06 18:39:16 +0300

    Добавил отдельно обработку событий в игре

--- life/main.py
+++ life/main.py
@@ -1,8 +1,8 @@
 import pygame
-import scoreobj
 import menu
-import button
 import field
+import colours
+import event_handler
 
 
 gamefield = field.Field()
@@ -10,7 +10,7 @@ gamefield = field.Field()
 # ======== Инициализируем игровое окно  =============
 pygame.init()
 screen = pygame.display.set_mode(gamefield.field_size)
-screen.fill(field.Field.BACKGROUND)
+screen.fill(colours.BACKGROUND)
 pygame.display.set_caption('Convey`s game of Life')
 clock = pygame.time.Clock()
 # ===================================================
@@ -20,54 +20,32 @@ clock = pygame.time.Clock()
 menu = menu.Menu(gamefield)
 menu.draw(screen)
 
-score = scoreobj.ScoreObj(  # Счётчик живых клеток
-    (button.Button.widht + button.Button.margin) * 3,
-    gamefield.field_size[1] - field.Field.menu_height
-)
+handler = event_handler.EventHandler()
 
 done = False
-mode = 'draw'  # В игре четыре положения: draw, evolution, restart, quit
+mode = 'draw'  # В игре пять положения: draw, evolution, restart, quit, pause
 # ===================================================
 
 
 # ======== Основной цикл игры ===========
 while not done:
-    # Обработка основных событий игры
-    for event in pygame.event.get():
-        if event.type == pygame.MOUSEBUTTONDOWN:
-            mouse_pos = pygame.mouse.get_pos()
 
-            if mode == 'draw' and mouse_pos[1] < gamefield.field_size[1] - field.Field.menu_height:
-                # print(mouse_pos)
-                gamefield.click(mouse_pos)
-
-            if mouse_pos[1] > gamefield.field_size[1] - field.Field.menu_height:
-                mode = menu.click(mouse_pos, mode)
-        if event.type == pygame.QUIT or mode == 'quit':
-            done = True
-        elif event.type == pygame.KEYDOWN and mode == 'draw':
-            if event.key == pygame.K_RETURN:
-                mode = 'evolution'  # начало эволюции
-                print('evolution mode on')
-
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_ESCAPE:
-                done = True
+    mode, done = handler.event_handler(pygame.event.get(), mode, done, gamefield, menu)
 
     # Обработка состояний игры
-    if mode == 'draw':
+    if mode == 'draw' or mode == 'pause':
         gamefield.draw(screen)  # Отрисовка живых клеток
         clock.tick(30)
 
     elif mode == 'restart':
-        screen.fill(field.Field.BACKGROUND)  # Обновление игрового поля
+        screen.fill(colours.BACKGROUND)  # Обновление игрового поля
         gamefield.refresh()
         mode = 'draw'
 
     elif mode == 'evolution':
         gamefield.evolution_step()  # Один шаг эволюции
         gamefield.draw(screen)
-        clock.tick(6)
+        clock.tick(handler.evolution_tick)
 
     elif mode == 'gameover':
         gamefield.game_over(screen)  # Конец игры (При нажатии restart игра возобновится)
@@ -76,7 +54,6 @@ while not done:
         mode = 'gameover'  # Изменение состояния на gameover (по правилам игры)
 
     menu.draw(screen)
-    score.draw(screen, gamefield)
 
     pygame.display.flip()
 
