9350255c90a7b20a7cd26959b4caec2fa745c44e
zemerov <37634929+zemerov@users.noreply.github.com>
2019-04-06 18:36:22 +0300

    Update menu.py

--- life/menu.py
+++ life/menu.py
@@ -1,15 +1,16 @@
-import pygame
 import field
 import button
 
 
 class Menu:
-    """В Menu опишем 3 кнопки start, restart, quit"""
+    """В Menu опишем 4 кнопки start, restart, quit, pause"""
 
     FONT = 'calibri'
     FONT_SIZE = 36
 
     def __init__(self, game_f):
+        self.prev_state = 'draw'
+
         self.start = button.Button(
             'start',
             Menu.FONT,
@@ -34,8 +35,16 @@ class Menu:
             game_f.field_size[1] - field.Field.menu_height
         )
 
+        self.pause = button.Button(
+            'pause',
+            Menu.FONT,
+            Menu.FONT_SIZE,
+            (button.Button.widht + button.Button.margin) * 3,
+            game_f.field_size[1] - field.Field.menu_height
+        )
+
     def draw(self, surface):
-        for but in (self.start, self.restart, self.quit):
+        for but in (self.start, self.restart, self.quit, self.pause):
             but.draw(surface)
 
     def click(self, pos, state):
@@ -49,4 +58,15 @@ class Menu:
                 button.Button.widht * 3 + button.Button.margin * 2
         ):
             return 'quit'
+        elif (
+                button.Button.widht * 3 + button.Button.margin * 2 <
+                pos[0] <
+                button.Button.widht * 4 + button.Button.margin * 3
+        ):
+            if state == 'evolution':
+                self.prev_state = state
+                return 'pause'
+            elif state == 'pause':
+                return 'evolution'
+
         return state
