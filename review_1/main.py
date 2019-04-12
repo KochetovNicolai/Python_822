2c4b0b35955c8d4e44b705618cce32876e1d0250
tanya124 <47141894+tanya124@users.noreply.github.com>
2019-04-13 00:20:32 +0300

    Update main.py

--- review_1/main.py
+++ review_1/main.py
@@ -1,117 +1,88 @@
 from tkinter import *
+import os
+from functools import partial
 
-score = 0
+class Window:
+    def __init__(self, root, update):
+        self.root = root
+        self.update = update
+        self.root.configure(background='#eafffa')
 
-def updateScoreSpace(event):
-    global score
-    score += 1
-    scoreLabel.config(text="Очки:  {}".format(score))
+        self.lowFrame = Frame(root)
+        self.canvas = Canvas(self.lowFrame, width=250, height=400, background='#eafffa')
+        self.frame = Frame(self.canvas)
+        self.myscrollbar = Scrollbar(self.lowFrame, orient='vertical', command=self.canvas.yview)
+        self.canvas.configure(yscrollcommand=self.myscrollbar.set)
+        self.canvas.create_window((0,0), window = self.frame)
+        def conf(event):
+            self.canvas.configure(scrollregion=self.canvas.bbox('all'))
+        self.frame.bind('<Configure>', conf)
+        self.lowFrame.pack(side='right')
+        self.myscrollbar.pack(side='right', fill=Y)
+        self.canvas.pack()
 
-def updateScore(): 
-    global score
-    score += 1
-    scoreLabel.config(text="Очки:  {}".format(score))
+        self.shop = PhotoImage(file=os.path.join('images', 'shop.png'))
+        self.shopLabel = Label(root, image=self.shop, background='#eafffa')
+        self.shopLabel.place(relx=0.75)
 
-def get_autoClicker(n):
-    global score
-    if n == 1:
-        if score >= 20:
-            score -= 20
-            autoClicker(n)
-        else:
-            pass
-    elif n == 3:
-        if score >= 100:
-            score -= 100
-            autoClicker(n)
-        else:
-            pass
-    elif n == 5:
-        if score >= 500:
-            score -= 500
-            autoClicker(n)
-        else:
-            pass
-    elif n == 10:
-        if score >= 3000:
-            score -= 3000
-            autoClicker(n)
-        else:
-            pass
-    elif n == 30:
-        if score >= 10000:
-            score -= 10000
-            autoClicker(n)
-        else:
-            pass
+        self.inscription = PhotoImage(file=os.path.join('images', 'shop1.png'))
+        self.inscriptionLabel = Label(root, image=self.inscription, background='#eafffa')
+        self.inscriptionLabel.place(relx=0.75, rely=0.85)
 
-def autoClicker(n):
-    global score
-    score += n
-    scoreLabel.config(text="Очки:  {}".format(score))
-    root.after(30000, autoClicker, n)
+        self.line = PhotoImage(file=os.path.join('images', 'line.png'))
+        self.lineLabel = Label(root, image=self.line)
+        self.lineLabel.place(relx=0.7)
+
+        self.scoreLabel = Label(root, text="Очки: 0", font=("Helvetica", 12), background='#eafffa')
+        self.scoreLabel.place(relx=0.3, rely=0.05)
+
+        self.cookie = PhotoImage(file=os.path.join('images', 'cookie.png'))
+        self.updateScore = partial(update.updateScore, self)
+        self.cook = Button(root, image=self.cookie, background='#eafffa', activebackground='#eafffa', bd=0, command=self.updateScore)
+        self.cook.place(relx=0.2, rely=0.2)
+
+        self.buttons = [PhotoImage(file=os.path.join('images', 'Button1.png')),
+                        PhotoImage(file=os.path.join('images', 'Button2.png')),
+                        PhotoImage(file=os.path.join('images', 'Button3.png')),
+                        PhotoImage(file=os.path.join('images', 'Button4.png')),
+                        PhotoImage(file=os.path.join('images', 'Button5.png'))]
+        self.cursors = [ Button(self.frame, image=i, bd=0, background='#eafffa') for i in self.buttons ]
+
+
+        self.price = [[1,20], [3,100], [5,500], [10,3000], [30,10000]]
+        self.k = 0 #для итерации по price
+        for i in self.cursors:
+            self.buy = partial(update.buy, self, self.price[self.k][1], self.price[self.k][0])
+            self.k += 1
+            i.configure(command=self.buy)
+        for i in self.cursors:
+            i.pack()
+
+
+class Update:
+    def __init__(self):
+        self.score = 0
+
+    def updateScore(self, object):
+        self.score += 1
+        object.scoreLabel.config(text="Очки: {}".format(self.score))
+
+    def buy(self, object, cost, amount_click):
+        if self.score >= cost:
+            self.score -= cost
+            self.autoClicker(object, amount_click)
+
+    def autoClicker(self, object, n):
+        self.score += n
+        object.scoreLabel.config(text="Очки: {}".format(self.score))
+        root.after(1000, self.autoClicker, object, n) #30 игровых секунд = 1 секунде (чтобы быстрее проверять работоспособность функции)
+
+
 
 root = Tk()
 root.title("Кликер")
 root.geometry("1000x600")
-root.configure(background='#eafffa')
-
-lowFrame = Frame(root) 
-
-canvas = Canvas(lowFrame, width=250, height=400, background='#eafffa')
-frame = Frame(canvas)
-myscrollbar = Scrollbar(lowFrame, orient = 'vertical', command = canvas.yview)
-canvas.configure(yscrollcommand = myscrollbar.set)
-
-canvas.create_window((0,0), window = frame)
-
-def conf(event):
-    canvas.configure(scrollregion = canvas.bbox('all'))
-frame.bind('<Configure>', conf)
-
-shop = PhotoImage(file="images\shop.png")
-shopLabel = Label(root, image=shop,background='#eafffa')
-
-inscription = PhotoImage(file="images\shop1.png")
-inscriptionLabel = Label(root, image=inscription, background='#eafffa')
-
-line = PhotoImage(file="images\line.png")
-lineLabel = Label(root, image=line)
-
-scoreLabel = Label(root, text="Очки: " + str(score), font=("Helvetica", 12), background='#eafffa')
-
-cookie = PhotoImage(file="images\cookie.png")
-cook = Button(root, image=cookie, background='#eafffa', activebackground='#eafffa', bd=0, command = updateScore)
-
-
-button1 = PhotoImage(file="images\Button1.png")
-button2 = PhotoImage(file="images\Button2.png")
-button3 = PhotoImage(file="images\Button3.png")
-button4 = PhotoImage(file="images\Button4.png")
-button5 = PhotoImage(file="images\Button5.png")
-#кнопки магазина
-cursor1 = Button(frame, image=button1, bd=0, background='#eafffa', command=lambda: get_autoClicker(1))
-cursor2 = Button(frame, image=button2, bd=0, background='#eafffa', command=lambda: get_autoClicker(3))
-cursor3 = Button(frame, image=button3, bd=0, background='#eafffa', command=lambda: get_autoClicker(5))
-cursor4 = Button(frame, image=button4, bd=0, background='#eafffa', command=lambda: get_autoClicker(10))
-cursor5 = Button(frame, image=button5, bd=0, background='#eafffa', command=lambda: get_autoClicker(30))
-
-root.bind('<space>', updateScoreSpace) 
-
-lowFrame.pack(side = 'right')
-myscrollbar.pack(side = 'right', fill = Y)
-canvas.pack()
-
-inscriptionLabel.place(x=760, y=505)
-scoreLabel.place(x = 265, y = 25)
-cook.place(x = 150, y = 120)
-shopLabel.place(x = 750, y = 10)
-lineLabel.place(x=720)
-
-cursor1.pack()
-cursor2.pack()
-cursor3.pack()
-cursor4.pack()
-cursor5.pack()
-
+root.resizable(False, False)
+update = Update()
+Window(root, update)
 root.mainloop()
