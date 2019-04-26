0cdcb8412c6533dc32fa04c439175ad0638f44d0
ARukhadze <44447352+AlRukhadze@users.noreply.github.com>
2019-04-26 09:03:15 +0300

    Update visualisation.py

--- visualisation.py
+++ visualisation.py
@@ -23,11 +23,22 @@ class Visual:
         self.clr_lightseagreen = (32, 178, 170)     # цвет ячейки, фона при победе игрока и текста на кнопке "Try again"
         self.clr_lavenderblush = (255, 240, 245)    # цвет цифр, текста, кнопки для начала новой игры
         self.clr_white = (255, 255, 255)            # цвет границ между ячейками
+        self.clr_red = (255, 0, 0)
 
         # объявление используемых шрифтов
         self.big_font = pygame.font.Font(None, self.cell_size)
         self.standard_font = pygame.font.Font(None, self.cell_size // 2)
         self.small_font = pygame.font.Font(None, self.cell_size // 4)
+
+        # координаты, текст для отрисовки финального экрана и межстрочные интервалы
+        self.pos_x = self.left + self.cell_size * 2
+        self.pos_y = self.top + self.cell_size * 1
+        self.text_soluble = ['Congratulations,', 'you won!', 'Your score: {}']
+        self.text_insoluble = ['Oh, someone has confused all the chips,', 'and the task became intractable!',
+                               'But you fought like a lion and brought', 'the game to the end. You won!',
+                               'Your score: {}']
+        self.interval_soluble = 0.5 * self.cell_size
+        self.interval_insoluble = 0.3 * self.cell_size
 
     # отрисовка ячейки
     def draw_cell(self, clr, rind, cind):
@@ -94,63 +105,39 @@ class Visual:
     def draw_try_again_button(self):
         pygame.draw.rect(self.screen, self.clr_lavenderblush,           # отрисовываем поле для "кнопки"
                          (self.left + 0.7 * self.cell_size,
-                          self.top + 2.6 * self.cell_size,
+                          self.top + 3.1 * self.cell_size,
                           self.cell_size * 2.6,
                           self.cell_size * 0.8))
         text = self.standard_font.render('Try again :)', 1, self.clr_lightseagreen)
-        pos_x = self.left + 1.1 * self.cell_size
-        pos_y = self.top + 2.85 * self.cell_size
-        self.screen.blit(text, (pos_x, pos_y))
+        pos_x = self.left + 2 * self.cell_size
+        pos_y = self.top + 3.5 * self.cell_size
+        place = text.get_rect(center=(pos_x, pos_y))
+        self.screen.blit(text, place)
 
     # отрисовка поля в случае победы игрока, если комбинация была разрешима
     def win_screen_if_soluble(self, points):
         self.screen.fill(self.clr_lightseagreen)
-        text1 = self.standard_font.render('Congratulations,', 1, self.clr_lavenderblush)
-        text2 = self.standard_font.render('you won!', 1, self.clr_lavenderblush)
-        text3 = self.standard_font.render('Your score: {}'.format(points), 1, self.clr_lavenderblush)
-        pos_x1 = self.left + self.cell_size * 2
-        pos_y1 = self.top + self.cell_size * 1
-        pos_x2 = self.left + self.cell_size * 2.65
-        pos_y2 = self.top + self.cell_size * 1.5
-        pos_x3 = self.left + self.cell_size * 2.3
-        pos_y3 = self.top + self.cell_size * 2
-        place1 = text1.get_rect(center=(pos_x1, pos_y1))
-        place2 = text1.get_rect(center=(pos_x2, pos_y2))
-        place3 = text1.get_rect(center=(pos_x3, pos_y3))
-        self.screen.blit(text1, place1)
-        self.screen.blit(text2, place2)
-        self.screen.blit(text3, place3)
+        for i in range(len(self.text_soluble)):
+            if i == len(self.text_soluble) - 1:
+                string = self.standard_font.render(self.text_soluble[i].format(points), 1, self.clr_lavenderblush)
+            else:
+                string = self.standard_font.render(self.text_soluble[i], 1, self.clr_lavenderblush)
+            place = string.get_rect(center=(self.pos_x, self.pos_y + self.interval_soluble * i))
+            self.screen.blit(string, place)
         self.draw_try_again_button()
         pygame.display.flip()
 
     # отрисовка поля в случае победы игрока, если комбинация была неразрешима
     def win_screen_if_insoluble(self, points):
         self.screen.fill(self.clr_lightseagreen)
-        text1 = self.small_font.render('Oh, someone has confused all the chips,', 1, self.clr_lavenderblush)
-        text2 = self.small_font.render('and the task became intractable!', 1, self.clr_lavenderblush)
-        text3 = self.small_font.render('But you fought like a lion and brought', 1, self.clr_lavenderblush)
-        text4 = self.small_font.render('the game to the end. You won!', 1, self.clr_lavenderblush)
-        text5 = self.small_font.render('Your score: {}'.format(points), 1, self.clr_lavenderblush)
-        pos_x1 = self.left + self.cell_size * 2
-        pos_y1 = self.top + self.cell_size * 1.2
-        pos_x2 = self.left + self.cell_size * 2.3
-        pos_y2 = self.top + self.cell_size * 1.5
-        pos_x3 = self.left + self.cell_size * 2.1
-        pos_y3 = self.top + self.cell_size * 1.8
-        pos_x4 = self.left + self.cell_size * 2.4
-        pos_y4 = self.top + self.cell_size * 2.1
-        pos_x5 = self.left + self.cell_size * 3.1
-        pos_y5 = self.top + self.cell_size * 2.4
-        place1 = text1.get_rect(center=(pos_x1, pos_y1))
-        place2 = text1.get_rect(center=(pos_x2, pos_y2))
-        place3 = text1.get_rect(center=(pos_x3, pos_y3))
-        place4 = text1.get_rect(center=(pos_x4, pos_y4))
-        place5 = text1.get_rect(center=(pos_x5, pos_y5))
-        self.screen.blit(text1, place1)
-        self.screen.blit(text2, place2)
-        self.screen.blit(text3, place3)
-        self.screen.blit(text4, place4)
-        self.screen.blit(text5, place5)
+        self.screen.fill(self.clr_lightseagreen)
+        for i in range(len(self.text_insoluble)):
+            if i == len(self.text_insoluble) - 1:
+                string = self.small_font.render(self.text_insoluble[i].format(points), 1, self.clr_lavenderblush)
+            else:
+                string = self.small_font.render(self.text_insoluble[i], 1, self.clr_lavenderblush)
+            place = string.get_rect(center=(self.pos_x, self.pos_y + self.interval_insoluble * i))
+            self.screen.blit(string, place)
         self.draw_try_again_button()
         pygame.display.flip()
 
@@ -159,7 +146,7 @@ class Visual:
         pos_x = mouse_pos[0]
         pos_y = mouse_pos[1]
         if ((pos_x > self.left + 0.7 * self.cell_size) and (pos_x < self.left + 3.3 * self.cell_size)) \
-           and ((pos_y > self.top + 2.6 * self.cell_size) and (pos_y < self.top + 3.2 * self.cell_size)):
+           and ((pos_y > self.top + 3.1 * self.cell_size) and (pos_y < self.top + 3.9 * self.cell_size)):
             return True
         else:
             return False
