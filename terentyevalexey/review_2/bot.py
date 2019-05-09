09c94ed9b71dcb2b43197c729a49b52b0af5a4eb
alniterentev <terentev.an@phystech.edu>
2019-05-09 20:50:35 +0300

    fixed issues

--- terentyevalexey/review_2/bot.py
+++ terentyevalexey/review_2/bot.py
@@ -57,13 +57,21 @@ def talk(bot: Bot, update: Update):
 
 def ask(bot: Bot, update: Update):
     query = update.callback_query
-
     question_req = requests.get(
-        f"http://opentdb.com/api.php?amount=1&difficulty={query.data}").json()
-    if question_req['response_code'] != 0:
+        "http://opentdb.com/api.php",
+        params={'amount': '1',
+                'difficulty': query.data}
+    )
+    if question_req.status_code not in range(200, 300):
+        return
+    question_req = question_req.json()
+    if 'results' not in question_req:
         return
     question_req = question_req['results'][0]
-
+    if 'incorrect_answers' not in question_req or \
+            'correct_answer' not in question_req or \
+            'question' not in question_req:
+        return
     # answers choice keyboard
     answers = question_req['incorrect_answers']
     right_answer = question_req['correct_answer']
@@ -71,12 +79,14 @@ def ask(bot: Bot, update: Update):
 
     random.shuffle(answers)
 
+    # keyboard, correct answers send f'correct{answer}' to the callback_queue
+    # incorrect answers send f'incorrect{ans_num}{correct_answer}' to queue
     keyboard = InlineKeyboardMarkup([
         [InlineKeyboardButton(unescape(answer), callback_data=(
-                (
-                    'correct' if answer == right_answer else 'incorrect') + unescape(
-            right_answer))),
-         ] for answer in answers
+                ('correct' if answer == right_answer else
+                 f'incorrect{str(ans_num)}')
+                + unescape(right_answer))),
+         ] for ans_num, answer in enumerate(answers)
     ])
 
     query.edit_message_text(
@@ -92,6 +102,8 @@ def correct_answer(bot: Bot, update: Update):
          InlineKeyboardButton('Medium', callback_data='medium'),
          InlineKeyboardButton('Hard', callback_data='hard')],
     ])
+    # we use 7:: because the message starts with 'correct', we don't send
+    # ans_num because there is only one correct answer
     query.edit_message_text(
         text=unescape(query.message.text + '\nIndeed!' +
                       f'\n{query.data[7::]} - is the right answer!' +
@@ -107,9 +119,11 @@ def incorrect_answer(bot: Bot, update: Update):
          InlineKeyboardButton('Medium', callback_data='medium'),
          InlineKeyboardButton('Hard', callback_data='hard')],
     ])
+    # we use 10:: because the message starts with f'incorrect{ans_num}', where
+    # ans_num is less than 4 (this is to prevent same callback data and hacks)
     query.edit_message_text(
         text=unescape(query.message.text + '\nNo, you are wrong!' +
-                      f'\n{query.data[9::]} - is the correct answer.' +
+                      f'\n{query.data[10::]} - is the correct answer.' +
                       '\nMaybe, the next question will be easier?'),
         reply_markup=keyboard,
     )
@@ -120,18 +134,26 @@ def callback_handle(bot: Bot, update: Update):
     if query.data in ['easy', 'medium', 'hard']:
         ask(bot, update)
     elif query.data.startswith('correct'):
-        correct_answer(bot, update)
+        correct_answer(bot, update)  # the answer is correct - show it
     elif query.data.startswith('incorrect'):
-        incorrect_answer(bot, update)
+        incorrect_answer(bot, update)  # the answer is incorrect - show it
 
 
 def question_create(bot: Bot, update: Update):
     difficulty = update.message.text.split(None, 1)[0][1::]
     question_req = requests.get(
-        f"http://opentdb.com/api.php?amount=1&difficulty={difficulty}").json()
-    if question_req['response_code'] != 0:
-        return
+        "http://opentdb.com/api.php",
+        params={'amount': '1',
+                'difficulty': difficulty}
+    )
+
+    if question_req.status_code in range(200, 300):
+        question_req = question_req.json()
     question_req = question_req['results'][0]
+    if 'incorrect_answers' not in question_req or \
+            'correct_answer' not in question_req or \
+            'question' not in question_req:
+        return
     # answers choice keyboard
     answers = question_req['incorrect_answers']
     right_answer = question_req['correct_answer']
@@ -139,12 +161,14 @@ def question_create(bot: Bot, update: Update):
 
     random.shuffle(answers)
 
+    # keyboard, correct answers send f'correct{answer}' to the callback_queue
+    # incorrect answers send f'incorrect{ans_num}{correct_answer}' to queue
     keyboard = InlineKeyboardMarkup([
         [InlineKeyboardButton(unescape(answer), callback_data=(
-                (
-                    'correct' if answer == right_answer else 'incorrect') + unescape(
-            right_answer))),
-         ] for answer in answers
+                ('correct' if answer == right_answer else
+                 f'incorrect{str(ans_num)}')
+                + unescape(right_answer))),
+         ] for ans_num, answer in enumerate(answers)
     ])
 
     bot.send_message(
