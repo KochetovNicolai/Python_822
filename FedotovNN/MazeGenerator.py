5801ffd5a04a6254c45779c1be534945c8ffff0f
HellShef <krte97@mail.ru>
2019-05-21 22:38:57 +0300

    Update MazeGenerator.py

--- FedotovNN/MazeGenerator.py
+++ FedotovNN/MazeGenerator.py
@@ -3,62 +3,86 @@ import numpy as np
 import random
 pygame.init()
 
+def Get_Int():
+    flag=1
+    while True:
+        try:
+            x=int(input())
+        except ValueError:
+            print("Oops!  That was no valid number.  Try again...")
+            continue
+        if x<2:
+            print("Oops!  That was no valid number.  Try again...")
+            continue
+        break
+    return x
+
+def SetMaze(M):
+    print("input X resolution of Maze(>1)")
+    x=Get_Int()
+    M.SetXres(x)
+    print("input Y resolution of Maze(>1)")
+    x=Get_Int()
+    M.SetYres(x)
+
 class MAZE:
-    alg=1
-    xres = 50
-    yres = 50
-    colways = (0,0,0)
-    colwalls = (128,128,128)
-    colrightway = (200,200,200)
-    StartRes = 700
-    displayresx = StartRes
-    displayresy = StartRes
-    recx=(displayresx / xres)
-    recy=(displayresy / yres)
-    cellx = int((recx) * 0.8)
-    celly = int((recy) * 0.8)
-    Maze = np.zeros((yres+2, xres+2), dtype = bool)
-    Maze[:, :] = True
-    Maze[1 : yres + 1, 1 : xres + 1] = False
-    MazeWay=[]
-    MazeField=pygame.display.set_mode((displayresx,displayresy))
-    MazeField.fill(colwalls)
+
+    def __init__(self):
+            self.alg = 1
+            self.xres = 50
+            self.yres = 50
+            self.spaceamount = 0.8
+            self.podgonian = 16
+            self.colways = (0,0,0)
+            self.colwalls = (128,128,128)
+            self.colrightway = (200,200,200)
+            self.StartRes = 700
+            self.displayresx = self.StartRes
+            self.displayresy = self.StartRes
+            self.recx = (self.displayresx / self.xres)
+            self.recy = (self.displayresy / self.yres)
+            self.cellx = int((self.recx) * self.spaceamount)
+            self.celly = int((self.recy) * self.spaceamount)
+            self.Maze = np.zeros((self.yres+2, self.xres+2), dtype = bool)
+            self.Maze[:, :] = True
+            self.Maze[1 : self.yres + 1, 1 : self.xres + 1] = False
+            self.MazeWay = []
+            self.MazeField = pygame.display.set_mode((self.displayresx,self.displayresy))
+            self.MazeField.fill(self.colwalls)
+
     def Update(self):
-        self.recx=(self.displayresx / self.xres)
-        self.recy=(self.displayresy / self.yres)
-        self.cellx = int((self.recx) * 0.8)
-        self.celly = int((self.recy) * 0.8)
+        self.recx = (self.displayresx / self.xres)
+        self.recy = (self.displayresy / self.yres)
+        self.cellx = int((self.recx) * self.spaceamount)
+        self.celly = int((self.recy) * self.spaceamount)
         self.Maze = np.zeros((self.yres+2, self.xres+2), dtype = bool)
         self.Maze[:, :] = True
         self.Maze[1 : self.yres + 1, 1 : self.xres + 1] = False
-        self.MazeWay=[]
-        self.MazeField=pygame.display.set_mode((self.displayresx,self.displayresy))
+        self.MazeWay = []
+        self.MazeField = pygame.display.set_mode((self.displayresx,self.displayresy))
         self.MazeField.fill(self.colwalls)
         pygame.display.update()
+
     def SetXres(self,x):
-        if x*16>self.StartRes:
+        if x*self.podgonian > self.StartRes:
             self.displayresx = self.StartRes
         else :
-            self.displayresx = x * 16
-        self.xres=x
+            self.displayresx = x * self.podgonian
+        self.xres = x
+
     def SetYres(self,x):
-        if x*16>self.StartRes:
+        if x*self.podgonian > self.StartRes:
             self.displayresy = self.StartRes
         else:
-            self.displayresy = x * 16
-        self.yres=x
-
-
-
-
+            self.displayresy = x * self.podgonian
+        self.yres = x
 
 def Coord(point,M):
     return (int(point[0]*M.recy) - (M.celly+M.recy) / 2, int(point[1] * M.recx) - (M.cellx + M.recx) / 2)
 
-
 def DrowConnection(point1,point2,M,color):
 
-    pygame.time.delay(2)
+    pygame.time.delay(1)
     col = color
     dx=point1[1]-point2[1]
     dy=point1[0]-point2[0]
@@ -93,14 +117,12 @@ def InitMaze(M):
     DrowMaze(M)
 
 def DrowMaze(M):
-
-
     M.MazeField.fill(M.colwalls)
 
     for i in range(1, M.yres + 1 ):
         for j in range(1, M.xres + 1):
             (y, x)=Coord((i, j),M)
-            pygame.draw.rect(M.MazeField,(128,128,128),(x, y, M.cellx, M.celly))
+            pygame.draw.rect(M.MazeField,M.colwalls,(x, y, M.cellx, M.celly))
 
     for i in M.MazeWay:
         DrowConnection(i[0],i[1],M,M.colways)
@@ -154,9 +176,6 @@ def InitMazeDFS(M):
         now=StepBack(now,M)
         back=StepBack(now,M)
 
-
-
-
 def InitMazePrim(M):
 
     M.Maze[:, :] = True
@@ -180,31 +199,40 @@ def InitMazePrim(M):
                 walls.add((i,now))
             M.MazeWay+=[wall]
 
-
-
 def ReadMaze(M):
     M.MazeWay=[]
-    file=open("LastM.Maze.txt","r")
+    try:
+        file=open("LastMaze.txt","r")
+    except IOError:
+        print("Can't read maze.")
+        return
+    print("Maze was readed.")
     lines=file.readlines()
     for i in lines:
         n = i.split(' ')
-        M.MazeWay+=[((int(n[0]),int(n[1])),(int(n[2]),int(n[3])))]
+        try:
+            M.MazeWay+=[((int(n[0]),int(n[1])),(int(n[2]),int(n[3])))]
+        except:
+            print("Can't read maze.")
+            return
     file.close();
     DrowMaze(M)
 
 def WriteMaze(M):
-    file=open("LastM.Maze.txt","w")
+    file=open("LastMaze.txt","w")
     for i in M.MazeWay:
         file.write(str(i[0][0])+" "+str(i[0][1])+" "+str(i[1][0])+" "+str(i[1][1])+"\n")
     file.close()
 
-
-
-
-
 run = True
 M=MAZE()
-print("Hello!\n Press Q or E to switch mod\nPress W to read last M.Maze\nPress SPACE to Start Drawing\nPress F to draw right way\nA to set resolution\nEnjoy!")
+print('''Hello!
+Press Q or E to switch mod
+Press W to read last M.Maze
+Press SPACE to Start Drawing
+Press F to draw right way
+A to set resolution
+Enjoy!''')
 while run:
     pygame.time.delay(100)
     for event in pygame.event.get():
@@ -218,12 +246,7 @@ while run:
     if keys[pygame.K_w]:
         ReadMaze(M)
     if keys[pygame.K_a]:
-        print("input X resolution of Maze")
-        x=int(input())
-        M.SetXres(x)
-        print("input Y resolution of Maze")
-        x=int(input())
-        M.SetYres(x)
+        SetMaze(M)
         M.Update()
     if keys[pygame.K_f]:
         if len(M.MazeWay) == 0:
