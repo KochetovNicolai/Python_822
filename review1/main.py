1ad5a71e8c2cc8d6e9b7b3cf624bc46179fd8b8f
TheNorthRemember <48762307+TheNorthRemember@users.noreply.github.com>
2019-05-23 00:03:46 +0300

    Update main.py

--- review1/main.py
+++ review1/main.py
@@ -6,28 +6,28 @@ from classes import items
 import text, vars
 
 
-def event_processing(i):
-    print(i)
-    if i.type == pygame.QUIT:
-        vars.not_done = False
-    elif i.type == pygame.KEYDOWN:
-        if i.unicode == ' ':
-            print(vars.cookies)
-            vars.cookies+= vars.cookies_per_click
-            vars.clicks+=1
-            vars.all_cookies+= vars.cookies_per_click
-    elif i.type == Income:
-        vars.cookies+= vars.cookies_income
-        vars.all_cookies+= vars.cookies_income
-    elif (i.type == pygame.MOUSEBUTTONUP):
-        for j in items:
-            if pygame.Rect.collidepoint(j.buy_rec, i.pos):
-                j.buy(1)
-            elif pygame.Rect.collidepoint(j.buy10_rect, i.pos):
-                j.buy(10)
-            elif pygame.Rect.collidepoint(j.buymax_rect, i.pos):
-                while j.cost <= vars.cookies:
-                    j.buy(1)
+def event_processing(event):
+    print(event)
+    if event.type == pygame.QUIT:
+        vars.accumulative.not_done = False
+    elif event.type == pygame.KEYDOWN:
+        if event.key == pygame.K_SPACE:
+            print(vars.accumulative.cookies)
+            vars.accumulative.cookies+= vars.accumulative.cookies_per_click
+            vars.accumulative.clicks+=1
+            vars.accumulative.all_cookies+= vars.accumulative.cookies_per_click
+    elif event.type == Income:
+        vars.accumulative.cookies+= vars.accumulative.cookies_income
+        vars.accumulative.all_cookies+= vars.accumulative.cookies_income
+    elif (event.type == pygame.MOUSEBUTTONUP):
+        for i in items:
+            if pygame.Rect.collidepoint(i.buy_rec, event.pos):
+                i.buy(1)
+            elif pygame.Rect.collidepoint(i.buy10_rect, event.pos):
+                i.buy(10)
+            elif pygame.Rect.collidepoint(i.buymax_rect, event.pos):
+                while i.cost <= vars.accumulative.cookies:
+                    i.buy(1)
 
 screen = pygame.display.set_mode((800, 600))
 pygame.display.set_caption("Clicker")
@@ -35,12 +35,12 @@ print(pygame.font.get_fonts())
 clock = pygame.time.Clock()
 Income = pygame.USEREVENT + 1
 pygame.time.set_timer(Income, 1000)
-while vars.not_done:
+while vars.accumulative.not_done:
     text.screen_update(screen,0)
     pos = pygame.mouse.get_pos()
     for j in items:
         if pygame.Rect.collidepoint(j.buy_rec, pos):
-            j.is_pointed(pos)
+            j.on_point(pos)
     pygame.display.update()
     for i in pygame.event.get():
         event_processing(i)
