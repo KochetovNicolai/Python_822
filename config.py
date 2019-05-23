4d5e4d9d0ed8425813fa8052953ec0c798ea3550
AlRukhadze <rukhadze.ag@phystech.edu>
2019-05-23 08:16:24 +0300

    deleted new files

--- config.py
+++
@@ -1,8 +0,0 @@
-import os
-
-
-basedir = os.path.abspath(os.path.dirname(__name__))
-SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'database.db')
-SQLALCHEMY_TRACK_MODIFICATIONS = False
-
-CSRF_ENABLED = True
