bf836fc1eed15d23173339385987e5a3beb717f3
AlRukhadze <rukhadze.ag@phystech.edu>
2019-05-23 08:13:32 +0300

    added new files

---
+++ first_pancake.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+from app import app
+
+
+if __name__ == "__main__":				# check whether the executable file is the main program
+    app.run()							# and run the application
+
+
+# to do:
+# take information from the boxes and save it in a database
+# showing information about person's deadlines
